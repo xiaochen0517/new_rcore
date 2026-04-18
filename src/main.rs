d9982ee1757bb64d28b9f9a8cf335682491e2223
mochen <2827075398@qq.com>
2026-04-18 18:34:14 +0800

    并发错误导致最终结果不符合预期

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::{ptr::write_volatile, thread};
+
 fn main() {
-    println!("Hello, RCore!");
+    unsafe {
+        let mut num = 0u32;
+        let addr = (&raw mut num as *mut u32) as usize;
+        let mut thread_vec = Vec::new();
+        for _ in 0..10 {
+            let handle = thread::spawn(move || unsafe {
+                let num_addr = addr as *mut u32;
+                for _ in 0..100_000 {
+                    write_volatile(num_addr, *num_addr + 1);
+                }
+            });
+            thread_vec.push(handle);
+        }
+        for handle in thread_vec {
+            handle.join().unwrap();
+        }
+        println!("Final value of num: {}; expected: {}", num, 10 * 100_000);
+    }
 }
