2ef15a7ed56346461e01c967f4acd6a63cca12c8
mochen <2827075398@qq.com>
2026-04-18 18:25:33 +0800

    移除不必要的代码，简化主函数实现，更新配置文件

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,3 @@
-#![no_std]
-#![no_main]
-
-use core::panic::PanicInfo;
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-    let msg = b"Hello, RCore!\n";
-    unsafe {
-        core::arch::asm!(
-            "syscall",
-            in("rax") 1usize, // sys_write
-            in("rdi") 1usize, // stdout
-            in("rsi") msg.as_ptr(),
-            in("rdx") msg.len(),
-        );
-        core::arch::asm!(
-            "syscall",
-            in("rax") 60usize, // sys_exit
-            in("rdi") 0usize, // exit code
-        );
-    }
-    loop {}
+fn main() {
+    println!("Hello, RCore!");
 }
