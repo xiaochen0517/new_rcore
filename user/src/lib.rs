b13e1a2b78bbe24810d467f40a626d0262460678
mochenya <2827075398@qq.com>
2026-04-21 17:55:17 +0800

    新增 user_lib 模块及其依赖，更新 Cargo.toml 和 Cargo.lock 文件；重构 lib.rs 以实现系统启动和清理 BSS 段功能

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -1,14 +1,26 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![no_std]
+#![no_main]
+#![feature(linkage)]
+
+#[unsafe(no_mangle)]
+#[unsafe(link_section = ".text.entry")]
+pub extern "C" fn _start() -> ! {
+    clear_bss();
+    // exit(main());
+    panic!("Should never reach here!");
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[linkage = "weak"]
+#[unsafe(no_mangle)]
+pub extern "C" fn main() -> i32 {
+    panic!("main function not defined!");
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+fn clear_bss() {
+    unsafe extern "C" {
+        fn sbss();
+        fn ebss();
     }
+    (sbss as *const () as usize..ebss as *const () as usize)
+        .for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
 }
